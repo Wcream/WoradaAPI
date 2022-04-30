cbd9efe0e6d0c15c7625d489da41ea4a421d9783
woradaran <57227460+woradaran@users.noreply.github.com>
2022-04-30 14:59:20 +0700

    final

--- HW4/HW4_test.py
+++
@@ -1,5 +0,0 @@
-import requests
-URL ="http://127.0.0.1:5000/weather"
-
-response = requests.get(URL)
-print(response.json())
--- HW6/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
